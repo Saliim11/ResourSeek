63ba4af88bb8b489419f0be2c97591f8386780b0
Saliim11 <qalbunsaliimbakhri@gmail.com>
2023-12-22 19:56:34 +0700

    tambahan list buat home supplier

--- lib/remoteService.dart
+++ lib/remoteService.dart
@@ -2,11 +2,12 @@ import 'package:http/http.dart' as http;
 import 'package:resourseek_app/model/produk.dart';
 
 class RemoteServices {
-  // String host = 'http://localhost:8080';
+  String host = 'http://localhost:8080';
+
   Future<List<Produk>?> getAllProduk() async {
     var client = http.Client();
 
-    var uri = Uri.parse('http://localhost:8080/getAllProduk');
+    var uri = Uri.parse(host+'/getAllProduk');
     var response = await client.get(uri);
     if (response.statusCode == 200) {
       var json = response.body;
@@ -15,4 +16,18 @@ class RemoteServices {
       return null;
     }
   }
+
+  Future<List<Produk>?> getProdukByToko(String id) async {
+    var client = http.Client();
+
+    var uri = Uri.parse('http://localhost:8080/getProdukByIdToko/$id');
+    var response = await client.get(uri);
+    if (response.statusCode == 200) {
+      var json = response.body;
+      return postFromJson(json);
+    }else {
+      return null;
+    }
+  }
+
 }
--- lib/supplier/supplierMainPage/supplierHomePage.dart
+++ lib/supplier/supplierMainPage/supplierHomePage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:resourseek_app/model/produk.dart';
+import 'package:resourseek_app/remoteService.dart';
 
 class SupplierHomePage extends StatefulWidget {
   const SupplierHomePage({super.key});
@@ -8,11 +10,107 @@ class SupplierHomePage extends StatefulWidget {
 }
 
 class _SupplierHomePageState extends State<SupplierHomePage> {
+
+  List<Produk>? listProduk = [];
+  var isLoaded = false;
+  String id = "1";
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getData();
+  }
+
+  getData() async{
+    // listProduk = await RemoteServices().getProdukByToko(id);
+    listProduk = await RemoteServices().getAllProduk();
+    if (listProduk != null) {
+      setState(() {
+        isLoaded = true;
+        print("data sudah berhasil untuk diambil");
+      });
+    }else {
+      print("gagal mengambil data produk");
+    }
+  }
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text("Supplier home"),
+        child: GridView.builder(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          crossAxisSpacing: 5,
+          mainAxisSpacing: 5
+          ),
+          itemCount: listProduk?.length, 
+          // itemCount: items.length,
+          itemBuilder: (BuildContext context, int index) {
+            return InkWell(
+              onTap: () {
+                
+              },
+              child: Card(
+                elevation: 10,
+                child: GridTile(
+                          child: Container(
+                color: Colors.white,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Expanded(
+                      child: Center(
+                        child: Image.asset(
+                          "assets/shoppingcart.png",
+                          fit: BoxFit.fill,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            listProduk![index].nama_p,
+                            // items[index]['name'],
+                            style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16.0,
+                            ),
+                          ),
+                          SizedBox(height: 4.0),
+                          Text(
+                            listProduk![index].deskripsi_p,
+                            // items[index]['city'],
+                            style: TextStyle(color: Colors.grey),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Align(
+                        alignment: Alignment.bottomRight,
+                        child: Text(
+                          "Rp.${listProduk![index].harga}",
+                          // items[index]['price'],
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16.0,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                          ),
+                        ),
+              ),
+            );
+          }),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
